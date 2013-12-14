3d5c1d4c480138130a70a1d726ed82a8596acdb2
selenluft <calinescu.elena_cornelia@yahoo.com>
2013-12-14 04:26:17 -0500

    Initializing app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CollectiveThinking::Application.config.secret_key_base = 'e8f2f162c74ca5cfc345d7d5f4d83bce7c0414529e61431a07abb6923920a2e7c078717acd547500f8826a0a9333d5c42f12255370bb4f53c5d53240320e0d7a'
